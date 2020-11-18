8ae510789a8ecca56fa35bdf3b721704670d6f03
Mads <maller2650@gmail.com>
2020-11-18 17:11:27 +0100

    -- test

--- src/main/java/com/joneikholm/productapi/controllers/SearchController.java
+++ src/main/java/com/joneikholm/productapi/controllers/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SearchController {
+    
     @PostMapping("api/search")
     public ResponseEntity<Result> getSearchResult(@RequestBody AJAXrequest req){
         System.out.println("api/search kaldet med " + req.getProduct());
